4133eb828d7d6510159d880028eae43bbb83fc06
Ajay-k9999 <ajaykundula@sltechnocrats.net>
2025-07-17 09:02:46 +0530

    "update"

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import Login from "./Pages/Login";
 import Quiz from "./Pages/quiz";
 import Register from "./Pages/Register";
 import Review from "./Pages/review";
-import Scorepage from "./Pages/Score";
-import { BrowserRouter as Router, Route, Routes,Navigate} from 'react-router-dom';
+import Scorepage from "./Pages/Score"; 
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to='/Login'/>}/>
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Register" element={<Register />} />
-        <Route path="/Quiz" element={<Quiz />} />
+        <Route path="/" element={<Navigate to='/login' />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/quiz" element={<Quiz />} />
         <Route path="/scorepage" element={<Scorepage />} />
-        <Route path="/review" element= {<Review />}/>
+        <Route path="/review" element={<Review />} />
       </Routes>
     </Router>
   );
