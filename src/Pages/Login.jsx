661f879ea9184740b783d5172d228dfff2dbc6e2
Ajay-k9999 <ajaykundula@sltechnocrats.net>
2025-07-18 17:33:41 +0530

    "update"

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -28,9 +28,9 @@ const Login =()=>{
         setUser({...user,[e.target.name]:e.target.value})
     };
     return( 
-        <div  style={{display: "flex",flexDirection: "column",alignItems: 'flex-end',justifyContent: 'flex-end',height:"100vh",width:"100%",backgroundImage:`url(${loginBg})`,backgroundSize:'cover',backgroundPosition:"center",paddingRight:"60px"}} >
+        <div  style={{display: "flex",flexDirection: "column",alignItems: 'flex-end',justifyContent: 'flex-end',height:"100vh",width:"100vw",backgroundImage:`url(${loginBg})`,backgroundSize:'cover',backgroundPosition:"center",paddingRight:"60px"}} >
             <h1 style={{font: "initial",fontWeight: 'bold',fontSize: "60px",textAlign: "center"}}>Login form</h1>
-            <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: 'column',alignItems: 'center',border: "1px solid #ccc",borderRadius: "7px",padding: "16px",width: "400px",height:"400px"}}>
+            <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: 'column',alignItems: 'center',border: "1px solid #ccc",borderRadius: "7px",padding: "16px",width: "500px",height:"300px"}}>
                 <input type="email" value={user.email} name="email" placeholder="Enter your email..." onChange={handlechange} required style={{marginTop:"20px" ,marginBottom: "20px", padding: "8px", width: "100%" ,borderRadius:"6px"}}/>
                 <input type="password" value={user.password} name="password" placeholder="Enter your password..." onChange={handlechange} required style={{ marginBottom: "20px", padding: "8px", width: "100%" ,borderRadius:"6px"}}/>
                 <button type="submit" style={{  padding: "8px 16px",color:"white",backgroundColor:"#2a52be  " }}>Submit</button>
