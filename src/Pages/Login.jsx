565e321fc8681b3b80ae0e33e9e0fd3271281ad5
Ajay-k9999 <ajaykundula@sltechnocrats.net>
2025-07-18 16:09:52 +0530

    "update"

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from "react";
 import axios from 'axios'
 import {useNavigate,Link} from 'react-router-dom';
+import image from '../Images/login.jpg'
 
 const Login =()=>{
     const[user,setUser]=useState({email:"",password:""})
@@ -26,12 +27,12 @@ const Login =()=>{
         setUser({...user,[e.target.name]:e.target.value})
     };
     return( 
-        <div style={{display: "flex",flexDirection: "column",alignItems: "center",justifyContent: "center",height:"100vh",width:"100vw"}}>
+        <div  style={{display: "flex",flexDirection: "column",alignItems: "center",justifyContent: "center",height:"100vh",width:"100vw",backgroundImage:`url(${image})`,backgroundSize:'cover'}} >
             <h1 style={{font: "initial",fontWeight: 'bold',fontSize: "60px",textAlign: "center"}}>Login form</h1>
             <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: 'column',alignItems: 'center',border: "1px solid #ccc",borderRadius: "7px",padding: "16px",width: "400px",height:"200px"}}>
-                <input type="email" value={user.email} name="email" placeholder="Enter your email..." onChange={handlechange} required style={{marginTop:"20px" ,marginBottom: "20px", padding: "8px", width: "100%" }}/>
-                <input type="password" value={user.password} name="password" placeholder="Enter your password..." onChange={handlechange} required style={{ marginBottom: "20px", padding: "8px", width: "100%" }}/>
-                <button type="submit" style={{  padding: "8px 16px" }}>Submit</button>
+                <input type="email" value={user.email} name="email" placeholder="Enter your email..." onChange={handlechange} required style={{marginTop:"20px" ,marginBottom: "20px", padding: "8px", width: "100%" ,borderRadius:"6px"}}/>
+                <input type="password" value={user.password} name="password" placeholder="Enter your password..." onChange={handlechange} required style={{ marginBottom: "20px", padding: "8px", width: "100%" ,borderRadius:"6px"}}/>
+                <button type="submit" style={{  padding: "8px 16px",color:"white",backgroundColor:"#2a52be  " }}>Submit</button>
             </form>
             <h5 style={{textAlign: 'center',marginTop: '16px'}}>Don’t have an account? <Link to='/Register'>Register</Link></h5></div>
             )
