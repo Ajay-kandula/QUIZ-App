bfba465106356b33ff51bd3b58c43886f803545c
Ajay-k9999 <ajaykundula@sltechnocrats.net>
2025-07-18 16:16:53 +0530

    "update"

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,8 @@
 import React,{useState} from "react";
 import axios from 'axios'
 import {useNavigate,Link} from 'react-router-dom';
-import image from '../Images'
+import loginBg from "../Images/login.jpg";
+
 
 const Login =()=>{
     const[user,setUser]=useState({email:"",password:""})
@@ -27,7 +28,7 @@ const Login =()=>{
         setUser({...user,[e.target.name]:e.target.value})
     };
     return( 
-        <div  style={{display: "flex",flexDirection: "column",alignItems: "center",justifyContent: "center",height:"100vh",width:"100vw",backgroundImage:`url(${image})`,backgroundSize:'cover'}} >
+        <div  style={{display: "flex",flexDirection: "column",alignItems: "center",justifyContent: "center",height:"100vh",width:"100vw",backgroundImage:`url(${loginBg})`,backgroundSize:'cover',backgroundPosition:"center"}} >
             <h1 style={{font: "initial",fontWeight: 'bold',fontSize: "60px",textAlign: "center"}}>Login form</h1>
             <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: 'column',alignItems: 'center',border: "1px solid #ccc",borderRadius: "7px",padding: "16px",width: "400px",height:"200px"}}>
                 <input type="email" value={user.email} name="email" placeholder="Enter your email..." onChange={handlechange} required style={{marginTop:"20px" ,marginBottom: "20px", padding: "8px", width: "100%" ,borderRadius:"6px"}}/>
