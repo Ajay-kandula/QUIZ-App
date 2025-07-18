f62e29bfae41c76b33ae91b73332941acfdb670c
Ajay-k9999 <ajaykundula@sltechnocrats.net>
2025-07-18 17:16:42 +0530

    "update"

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -28,7 +28,7 @@ const Login =()=>{
         setUser({...user,[e.target.name]:e.target.value})
     };
     return( 
-        <div  style={{display: "flex",flexDirection: "column",alignItems: 'centre',justifyContent: 'flex-end',height:"100vh",width:"100vw",backgroundImage:`url(${loginBg})`,backgroundSize:'cover',backgroundPosition:"center",marginRight:"10px"}} >
+        <div  style={{display: "flex",flexDirection: "column",alignItems: 'flex-end',justifyContent: 'flex-end',height:"100vh",width:"100vw",backgroundImage:`url(${loginBg})`,backgroundSize:'cover',backgroundPosition:"center",marginRight:"10px"}} >
             <h1 style={{font: "initial",fontWeight: 'bold',fontSize: "60px",textAlign: "center"}}>Login form</h1>
             <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: 'column',alignItems: 'center',border: "1px solid #ccc",borderRadius: "7px",padding: "16px",width: "400px",height:"200px"}}>
                 <input type="email" value={user.email} name="email" placeholder="Enter your email..." onChange={handlechange} required style={{marginTop:"20px" ,marginBottom: "20px", padding: "8px", width: "100%" ,borderRadius:"6px"}}/>
