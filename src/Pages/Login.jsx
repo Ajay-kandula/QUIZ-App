959cf27eca5f7741549e825d614d76e67b3c80a5
Ajay-k9999 <ajaykundula@sltechnocrats.net>
2025-07-18 17:44:17 +0530

    "update"

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -28,11 +28,11 @@ const Login =()=>{
         setUser({...user,[e.target.name]:e.target.value})
     };
     return( 
-        <div  style={{display: "flex",flexDirection: "column",alignItems: 'flex-end',justifyContent: 'flex-end',height:"100vh",width:"100vw",backgroundImage:`url(${loginBg})`,backgroundSize:'cover',backgroundPosition:"center",paddingRight:"70px"}} >
-            <h1 style={{font: "initial",fontWeight: 'bold',fontSize: "60px",textAlign: "center",paddingRight:"60px"}}>Login form</h1>
+        <div  style={{display: "flex",flexDirection: "column",alignItems: 'flex-end',justifyContent: 'flex-end',height:"100vh",width:"100vw",backgroundImage:`url(${loginBg})`,backgroundSize:'cover',backgroundPosition:"center",paddingRight:"80px"}} >
+            <h1 style={{font: "initial",fontWeight: 'bold',fontSize: "60px",textAlign: "center",paddingRight:"100px"}}>Login form</h1>
             <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: 'column',alignItems: 'center',border: "1px solid #ccc",borderRadius: "7px",padding: "16px",width: "500px",height:"300px"}}>
-                <input type="email" value={user.email} name="email" placeholder="Enter your email..." onChange={handlechange} required style={{marginTop:"40px" ,marginBottom: "40px", padding: "8px", width: "100%" ,borderRadius:"6px",}}/>
-                <input type="password" value={user.password} name="password" placeholder="Enter your password..." onChange={handlechange} required style={{ marginBottom: "40px", padding: "8px", width: "100%" ,borderRadius:"6px"}}/>
+                <input type="email" value={user.email} name="email" placeholder="Enter your email..." onChange={handlechange} required style={{marginTop:"40px" ,marginBottom: "40px", padding: "8px", width: "100%" ,borderRadius:"6px",border:'none'}}/>
+                <input type="password" value={user.password} name="password" placeholder="Enter your password..." onChange={handlechange} required style={{ marginBottom: "40px", padding: "8px", width: "100%" ,borderRadius:"6px",border:"none"}}/>
                 <button type="submit" style={{  padding: "8px 16px",color:"white",backgroundColor:"#2a52be  " }}>Submit</button>
             </form>
             <h5 style={{textAlign: 'center',marginTop: '16px',color:"red"}}>Don’t have an account? <Link to='/Register'>Register</Link></h5></div>
