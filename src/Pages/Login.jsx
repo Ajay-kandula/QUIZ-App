949a5956059f36f7998ffa23b9cd8ec16351e025
Ajay-k9999 <ajaykundula@sltechnocrats.net>
2025-07-23 09:50:02 +0530

    "update"

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -29,11 +29,11 @@ const Login =()=>{
     };
     return( 
         <div style={{display:'flex',justifyContent:"flex-start",flexDirection:"column",minHeight:"100vh",width:"100vw",alignItems:"flex-start",backgroundImage:`url(${login})`,backgroundPosition:'center',backgroundSize:"cover"}} >
-            <h1 style={{color:"white",marginBottom:"1px",marginLeft:'190px',marginTop:"5px"}}>Login form</h1>
+            <h1 style={{color:"white",marginBottom:"1px",marginLeft:'190px',marginTop:"40px"}}>Login form</h1>
             <div  style={{display:"flex",justifyContent:'center',alignItems:"center",marginBottom:"18px",height:"30%",width:"35%",flexDirection:"column",border:"1px solid #ccc",borderRadius:"6px",padding:"30px",textAlign:'center',marginLeft:"100px",marginTop:"50px"}}>
                 <form onSubmit={handleSubmit} >
-                    <input type="email" value={user.email} name="email" placeholder="Enter your email..." onChange={handlechange}  style={{marginBottom:"20px",border:'none',borderRadius:"6px",width:'70%',padding:"10px"}}/>
-                    <input type="password" value={user.password} name="password" placeholder="Enter your password..." onChange={handlechange} required style={{ marginBottom:"20px",border:"none",borderRadius:"6px",padding:"10px",width:"70%"}} /><br/>
+                    <input type="email" value={user.email} name="email" placeholder="Enter your email..." onChange={handlechange}  style={{marginBottom:"20px",border:'none',borderRadius:"6px",width:'80%',padding:"10px"}}/>
+                    <input type="password" value={user.password} name="password" placeholder="Enter your password..." onChange={handlechange} required style={{ marginBottom:"20px",border:"none",borderRadius:"6px",padding:"10px",width:"80%"}} /><br/>
                     <button type="submit" style={{  padding: "8px 16px",color:"white",backgroundColor:"#2a52be  ",width:"50%" }}>Submit</button>
             </form>
             </div>
