a9d7acacdb536e6d7a34e5030455a0a340bd40ea
Ajay-k9999 <ajaykundula@sltechnocrats.net>
2025-07-24 14:45:17 +0530

    "update"

--- src/Pages/Score.jsx
+++ src/Pages/Score.jsx
@@ -5,6 +5,7 @@ const Scorepage=()=>{
     const[score,setScore]=useState()
     const[total,setTotal]=useState()
     const navigate=useNavigate()
+    const isMobile=window.innerWidth<768;
     useEffect(()=>{
         const storedscore=localStorage.getItem('score');
         const totalQuestions = localStorage.getItem('total');
@@ -22,7 +23,7 @@ const Scorepage=()=>{
     return(
         <div style={{
       backgroundImage: `url("https://i.ibb.co/4wKbbgFf/Whats-App-Image-2025-07-23-at-16-24-29-a29f0fb2-compressed.webp")`,backgroundPosition:"center",backgroundSize:"cover",display:"flex",flexDirection:"column",justifyContent:"center",height:"100vh",width:"100vw",alignItems:"center",gap:"4px"}}>
-            <h1 style={{marginTop:"50px",marginBottom:'10px',color:'#006b3c'}}>Congratulations</h1>
+            <h1 style={{marginTop:isMobile?"50px":"50px",marginBottom:isMobile?"10px":'10px',color:'#006b3c'}}>Congratulations</h1>
             <h3 style={{marginBottom:"10px",color:"#daa520"}}>Your Quiz completed</h3>
             <h4 style={{marginTop:"3px",marginBottom:"10px",fontSize:"50px",color:"#ff2800"}}>Score</h4>
             <h2 style={{marginTop:"2px",fontSize:"30px",color:"#03c03c"}}>{score}/{total}</h2>
