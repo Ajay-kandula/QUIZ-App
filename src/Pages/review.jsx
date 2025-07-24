9218dbcd9692c36d7f1c7aa1cbadec5b0126b61d
Ajay-k9999 <ajaykundula@sltechnocrats.net>
2025-07-24 17:46:21 +0530

    "update"

--- src/Pages/review.jsx
+++ src/Pages/review.jsx
@@ -38,7 +38,7 @@ const Review=()=>{
                     <p>{ans.isCorrect? "correct":`Wrong | correctAnswer :${ans.correctAnswer}`}</p>
                 </div>
             ))};
-            <button onClick={handleExit} style={{color:"white",backgroundColor:"red",padding:"8px 12px",width:isMobile?"50px":"100px",marginLeft:isMobile?'50%':'50%'}}>Exit</button>
+            <button onClick={handleExit} style={{color:"white",backgroundColor:"red",padding:"8px 12px",width:isMobile?"50px":"100px",marginLeft:isMobile?'100px':'250%'}}>Exit</button>
         </div>
     )
 }
