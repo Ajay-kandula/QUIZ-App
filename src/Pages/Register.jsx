0cd360a6bb79eb549adc957010412f52ba4214b5
Ajay-k9999 <ajaykundula@sltechnocrats.net>
2025-07-24 09:18:52 +0530

    "update"

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -35,9 +35,9 @@ const Register= ()=>{
             <h1 style={{color:"white",marginBottom:"1px",marginLeft:isMobile?"15%":'15%',marginTop:isMobile?"40%":"60px",width:isMobile?"30%":"30%"}}>Register Page</h1>
             <div  style={{display:"flex",justifyContent:'center',alignItems:"center",marginBottom:"18px",height:"30%",width:isMobile?"35%":"35%",flexDirection:"column",border:"1px solid #ccc",borderRadius:"6px",padding:"30px",textAlign:'center',marginLeft:isMobile?"100px":"100px",marginTop:"50px"}}>
                 <form onSubmit={handleregister} >
-                    <input type="name" name="name" value={form.name} placeholder="Enter your name..." onChange={handlChange} required style={{marginTop:"20px",marginBottom:"18px",border:"none",padding:"10px",borderRadius:"6px",width:isMobile?"90%":"300px"}}/><br/>
-                    <input type="email" name="email" value={form.email} placeholder="Enter your email..." onChange={handlChange} required  style={{marginBottom:"20px",border:'none',borderRadius:"6px",width:isMobile?"90%":'300px',padding:"10px"}}/><br/>
-                    <input type="password" name="password" value={form.password} placeholder="Enter your password..." onChange={handlChange} required   style={{marginBottom:"20px",border:'none',borderRadius:"6px",width:isMobile?"90%":'300px',padding:"10px"}}/><br/>
+                    <input type="name" name="name" value={form.name} placeholder="Enter your name..." onChange={handlChange} required style={{marginTop:"20px",marginBottom:"18px",border:"none",padding:"10px",borderRadius:"6px",width:isMobile?"90%":"350px"}}/><br/>
+                    <input type="email" name="email" value={form.email} placeholder="Enter your email..." onChange={handlChange} required  style={{marginBottom:"20px",border:'none',borderRadius:"6px",width:isMobile?"90%":'350px',padding:"10px"}}/><br/>
+                    <input type="password" name="password" value={form.password} placeholder="Enter your password..." onChange={handlChange} required   style={{marginBottom:"20px",border:'none',borderRadius:"6px",width:isMobile?"90%":'350px',padding:"10px"}}/><br/>
                     <button type="submit" style={{  padding: "10px",color:"white",backgroundColor:"#2a52be  ",width:isMobile?"50%":"150px" }}> Submit</button>
                 </form>
             </div>
