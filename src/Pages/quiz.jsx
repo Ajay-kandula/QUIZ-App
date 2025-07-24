d5e78c6f35b955d6ffe67fe7954bd34a0087b802
Ajay-k9999 <ajaykundula@sltechnocrats.net>
2025-07-24 09:54:14 +0530

    "update"

--- src/Pages/quiz.jsx
+++ src/Pages/quiz.jsx
@@ -9,6 +9,7 @@ const Quiz = () => {
   const [timer, setTimer] = useState(30);
   const [answers, setAnswers] = useState({});
   const navigate = useNavigate();
+  const isMobile=window.innerWidth<768;
   const token = localStorage.getItem("token");
 
 
@@ -96,8 +97,8 @@ const Quiz = () => {
   const q = questions[current];
 
   return (
-    <div  style={{backgroundImage:`url("https://i.ibb.co/WvDS0xQd/iz-compressed.webp")`,backgroundPosition:"center",backgroundSize:"cover",height:"100vh",width:'100vw',display:"flex",justifyContent:"flex-start",flexDirection:"column",alignItems:"flex-start"}}>
-      <h4 style={{marginLeft:"1050px",marginBottom:"65px",fontSize:"25px"}}>Time Left: {timer}s</h4>
+    <div  style={{backgroundImage:`url("https://i.ibb.co/WvDS0xQd/iz-compressed.webp")`,backgroundPosition:"center",backgroundSize:"cover",height:isMobile?"100%":"100vh",width:isMobile?"100%":'100vw',display:"flex",justifyContent:"flex-start",flexDirection:"column",alignItems:"flex-start"}}>
+      <h4 style={{marginLeft:isMobile?"150px":"1050px",marginBottom:isMobile?"40px":"65px",fontSize:isMobile?"15px":"25px"}}>Time Left: {timer}s</h4>
       <div style={{display:"flex", flexDirection:"column",height:"80%",width:"600px",marginLeft:"500px",border:"2px solid grey",fontSize:"20px",borderRadius:"10px"}}>
         <h2 style={{textAlign:"center"}}>Q{current + 1}: {q.question}</h2>
         <ul style={{marginLeft:"10px",marginTop:"1px"}}>
