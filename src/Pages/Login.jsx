e70b6e636ca478a5c1d08fa1bcfae0a5c1c2b4ae
Ajay-k9999 <ajaykundula@sltechnocrats.net>
2025-07-24 08:47:53 +0530

    "update"

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -31,8 +31,8 @@ const Login =()=>{
     };
     return( 
         <div style={{display:'flex',justifyContent:"flex-start",flexDirection:"column",minHeight:"100vh",width:"100vw",alignItems:"flex-start",backgroundImage:`url("https://i.ibb.co/QvGWFSXJ/log-compressed.webp")`,backgroundPosition:'center',backgroundSize:"cover"}} >
-            <h1 style={{color:"white",marginBottom:"1px",marginLeft:isMobile ? "50px" : "190px",marginTop:"60px",width:isMobile ? "90%" : "30%",fontSize:isMobile ? "24px" : "40px"}}>Login form</h1>
-            <div  style={{display:"flex",justifyContent:'center',alignItems:"center",marginBottom:"18px",height:"30%",width:"35%",flexDirection:"column",border:"1px solid #ccc",borderRadius:"6px",padding:"30px",textAlign:'center',marginLeft:"100px",marginTop:"50px"}}>
+            <h1 style={{color:"white",marginBottom:"1px",marginLeft:isMobile ? "50px" : "190px",marginTop:isMobile? '30px':"60px",width:isMobile ? "90%" : "30%",fontSize:isMobile ? "24px" : "40px"}}>Login form</h1>
+            <div  style={{display:"flex",justifyContent:'center',alignItems:"center",marginBottom:"18px",height:"30%",width:"35%",flexDirection:"column",border:"1px solid #ccc",borderRadius:"6px",padding:"30px",textAlign:'center',marginLeft:isMobile?"20px":"100px",marginTop:isMobile?"50px":"50px"}}>
                 <form onSubmit={handleSubmit} >
                     <input type="email" value={user.email} name="email" placeholder="Enter your email..." onChange={handlechange}  style={{marginBottom:"20px",border:'none',borderRadius:"6px",width:'90%',padding:"10px"}}/>
                     <input type="password" value={user.password} name="password" placeholder="Enter your password..." onChange={handlechange} required style={{ marginBottom:"20px",border:"none",borderRadius:"6px",padding:"10px",width:"90%"}} /><br/>
